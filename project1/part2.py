dd59f506b7efebafd236c92353a7ac6039ed09e4
Fly <1756339062@qq.com>
2022-09-29 21:07:19 +0800

    feat(project1 part2): play the designated signal

---
+++ project1/part2.py
@@ -0,0 +1,22 @@
+'''
+reference:
+[1]https://stackoverflow.com/questions/30675731/howto-stream-numpy-array-into-pyaudio-stream
+'''
+import pyaudio
+import numpy as np
+
+duration = 3  # set the duration of the signal
+fs = 48 * 10**3  # set 48KHz
+samples = np.arange(0, duration, 1 / fs)
+signal = np.sin(2 * np.pi * 1000 * samples) + np.sin(
+    2 * np.pi * 10000 * samples)
+# sample = np.arange(0, 4, 1 / 3)
+# print(sample)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32, channels=1, rate=fs, output=True)
+data = signal.astype(np.float32).tostring()
+stream.write(data)
+stream.stop_stream()
+stream.close()
+p.terminate()
