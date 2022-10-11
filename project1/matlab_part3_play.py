11b620ab6742b131b1ef6e2c9209ffc55cce40fe
Fly <1756339062@qq.com>
2022-10-11 21:24:25 +0800

    refactor(project1 sender): pass

--- project1/matlab_part3_play.py
+++ project1/matlab_part3_play.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.typing import NDArray
 from scipy import integrate
-import pyaudio
+# import pyaudio
 import random
+import time
 import soundfile as sf
+import sounddevice as sd
 
 
 def read_data():
@@ -14,7 +16,10 @@ def read_data():
     return [temp[i:i + 100] for i in range(0, len(temp), 100)]
 
 
+t1 = time.time()
 frames = read_data()
+frames.append([random.random() for _ in range(100)])
+frames.append([random.random() for _ in range(100)])
 
 second = 0.001
 f = 48000
@@ -39,16 +44,21 @@ for i, frame in enumerate(frames):
                    j * 44] = carrier[j * 44:44 + j * 44] * (frame[j] * 2 - 1)
 
     frame_wave_pre = np.concatenate([preamble, frame_wave])
-    inter_space = np.zeros(random.randint(200, 400))
+    inter_space = np.zeros(random.randint(200, 200))
     output_frame: NDArray = np.concatenate([frame_wave_pre, inter_space])
     output_track.append(output_frame.astype(np.float32))
 
+output_track.append(np.array([random.random() for _ in range(200)]))
+
 sf.write('temp_out.wav', np.concatenate(output_track), samplerate=f)
 
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=f, output=True)
-for i, frame in enumerate(output_track):
-    stream.write(frame.tobytes())
+sd.play(np.concatenate(output_track), samplerate=f, blocking=True)
+# p = pyaudio.PyAudio()
+# stream = p.open(format=pyaudio.paFloat32, channels=1, rate=f, output=True)
+# for i, frame in enumerate(output_track):
+#     stream.write(frame.tobytes())
+t2 = time.time()
+print(f'time:{t2-t1}')
 
 if __name__ == '__main__':
     pass
