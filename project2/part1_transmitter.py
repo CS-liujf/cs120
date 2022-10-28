7a4bef053cddaebe0c278a773ee5795391c0adef
Fly <1756339062@qq.com>
2022-10-28 20:51:56 +0800

    feat(project2 sender): time limit

--- project2/part1_transmitter.py
+++ project2/part1_transmitter.py
@@ -1,18 +1,19 @@
-import struct
-from unicodedata import name
+'''
+https://www.cnpython.com/qa/934257
+'''
+import time
+import random
+import numpy as np
+from scipy import integrate
+import sounddevice as sd
 
-
-def byte_to_str(byte):
-    temp = int.from_bytes(byte, 'big')
-    b = bin(temp)[2:]
-    len_add = 8 - len(b)
-    return len_add * "0" + b
+NDArray = int
 
 
 def read_data():
     with open('./INPUT.bin', 'rb') as f:
         res = f.read()
-        bit_stream = ''.join([str(format(x, 'b')) for _, x in enumerate(res)])
+        bit_stream = ''.join(['{0:08b}'.format(x) for _, x in enumerate(res)])
 
     temp = [int(bit) for bit in bit_stream]
     return [temp[i:i + 100] for i in range(0, len(temp), 100)]
@@ -20,9 +21,57 @@ def read_data():
     # print(res[:10])
 
 
-if __name__ == '__main__':
-    import time
-    t1 = time.time()
-    read_data()
-    t2 = time.time()
-    print(t2 - t1)
+t1 = time.time()
+frames = read_data()
+frames.append([random.random() for _ in range(100)])
+frames.append([random.random() for _ in range(100)])
+
+second = 0.001
+f = 48000
+fc = 4_000
+bit_len = 2
+
+output_track: list[NDArray] = []
+output_track.append(np.zeros(random.randint(200, 201)))
+t = np.arange(0, 1, 1 / f)
+carrier = np.sin(2 * np.pi * fc * t)
+
+f_p = np.concatenate([
+    np.linspace(10_000 - 8000, 10_000, 220),
+    np.linspace(10_000, 10_000 - 8000, 220)
+])
+omega = 2 * np.pi * integrate.cumtrapz(f_p, t[0:440], initial=0)
+preamble = np.sin(omega)
+
+for i, frame in enumerate(frames):
+    frame_wave = np.zeros(len(frame) * bit_len)
+    for j in range(len(frame)):
+        frame_wave[j * bit_len:(j + 1) *
+                   bit_len] = carrier[j * bit_len:(j + 1) *
+                                      bit_len] * (frame[j] * 2 - 1)
+
+    frame_wave_pre = np.concatenate([preamble, frame_wave])
+    inter_space = np.zeros(random.randint(0, 50))
+    output_frame: NDArray = np.concatenate([frame_wave_pre, inter_space])
+    output_track.append(output_frame.astype(np.float32))
+
+output_track.append(np.array([random.random() for _ in range(200)]))
+
+# sf.write('temp_out.wav', np.concatenate(output_track), samplerate=f)
+
+sd.play(np.concatenate(output_track), samplerate=f, blocking=True)
+# p = pyaudio.PyAudio()
+# stream = p.open(format=pyaudio.paFloat32, channels=1, rate=f, output=True)
+# for i, frame in enumerate(output_track):
+#     stream.write(frame.tobytes())
+t2 = time.time()
+print(f'time:{t2-t1}')
+
+# if __name__ == '__main__':
+#     import time
+#     t1 = time.time()
+#     res = read_data()
+#     t2 = time.time()
+#     print(t2 - t1)
+#     # print(len(res))
+#     print(bytes(2))
