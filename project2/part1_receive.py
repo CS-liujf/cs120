1f542d02299948598667294a1b6ab1dc266252d8
Liuzhch1 <liuzhch1@shanghaitech.edu.cn>
2022-11-03 15:24:26 +0800

    feat(project2.part1_receive):change carrier wave

--- project2/part1_receive.py
+++ project2/part1_receive.py
@@ -13,11 +13,9 @@ total_duration = 16
 BIT_NUMBER = 50_000
 BODY_BIT_NUMBER = 100
 
-second = 0.001
-fc = 4_000
 
-t = np.arange(0, 1, 1 / F)
-carrier = np.sin(2 * np.pi * fc * t)
+SIGNAL_ONE = [-0.5, -0.5, -0.5, 0.5, 0.5, 0.5]
+SIGNAL_ZERO = [0.5, 0.5, 0.5, -0.5, -0.5, -0.5]
 
 
 def gene_preamble():
@@ -31,12 +29,15 @@ def gene_preamble():
 
 
 def record():
-    '''
+    """
     Record audio and store into wav file.
-    '''
+    """
     print('Recording...')
-    data = sd.rec(int(F*total_duration), samplerate=F,
-                  channels=CHANNELS, blocking=True, dtype='float32')
+    data = sd.rec(int(F * total_duration),
+                  samplerate=F,
+                  channels=CHANNELS,
+                  blocking=True,
+                  dtype='float32')
     print('Recording finished.')
 
     sf.write('tmp_received.wav', data, 48000)
@@ -46,23 +47,12 @@ def record():
     return data
 
 
-def smooth(a, WSZ):
-    # a: NumPy 1-D array containing the data to be smoothed
-    # WSZ: smoothing window size needs, which must be odd number,
-    # as in the original MATLAB implementation
-    out0 = np.convolve(a, np.ones(WSZ, dtype=int), 'valid')/WSZ
-    r = np.arange(1, WSZ-1, 2)
-    start = np.cumsum(a[:WSZ-1])[::2]/r
-    stop = (np.cumsum(a[:-WSZ:-1])[::2]/r)[::-1]
-    return np.concatenate((start, out0, stop))
-
-
 PREAMBLE_TRY_LENGTH = 500
 PREAMBLE_SIMILAR = 0.6
 
 start_time = time.time()
 
-bit_len = 4
+BIT_LEN = 6
 
 preamble = gene_preamble()
 preamble_len = len(preamble)  # 440
@@ -75,35 +65,34 @@ print("max_energy:", max_energy)
 
 index = 0
 final_bits = []
-for _ in range(BIT_NUMBER//BODY_BIT_NUMBER):
+for _ in range(BIT_NUMBER // BODY_BIT_NUMBER):
     if data_len - index < 0:
         break
     # package sync
     while True:
-        if index+PREAMBLE_TRY_LENGTH > data_len:
+        if index + PREAMBLE_TRY_LENGTH > data_len:
             break
-        corr = np.correlate(data[index:index+PREAMBLE_TRY_LENGTH], preamble)
+        corr = np.correlate(data[index:index + PREAMBLE_TRY_LENGTH], preamble)
         offset = -1
         max_index = np.argmax(corr)
-        if corr[max_index]/max_energy >= PREAMBLE_SIMILAR:
-            print("similarity:", corr[max_index]/max_energy, _)
+        if corr[max_index] / max_energy >= PREAMBLE_SIMILAR:
+            print("similarity:", corr[max_index] / max_energy, _)
             offset = max_index
         if offset == -1:
             # continue match
-            index += PREAMBLE_TRY_LENGTH-preamble_len
+            index += PREAMBLE_TRY_LENGTH - preamble_len
         else:
             # matched
             index += offset + preamble_len
             break
 
-    body_data = data[index:index+bit_len*100]
-    decode_removecarrier = body_data*carrier[:len(body_data)]
-    decode_power_bit = np.zeros(100)
-    for j in range(100):
-        decode_power_bit[j] = sum(decode_removecarrier[j*bit_len:2+j*bit_len])
+    body_data = data[index:index + BIT_LEN * BODY_BIT_NUMBER]
+    decode_power_bit = np.zeros(BODY_BIT_NUMBER)
+    for i in range(BODY_BIT_NUMBER):
+        decode_power_bit[i] = sum(
+            np.asarray(body_data[i * BIT_LEN:(i + 1) * BIT_LEN]) * SIGNAL_ONE)
     final_bits += [int(x) for x in decode_power_bit > 0]
-
 
 with open('output.txt', 'w') as f:
     f.write(''.join([str(x) for x in final_bits]))
-print("total time:", time.time()-start_time)
+print("total time:", time.time() - start_time)
