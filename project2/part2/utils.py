27ef8165e3127d50152c0f14c1adf9cd6a35b21d
Fly <1756339062@qq.com>
2022-11-04 14:56:28 +0800

    feat(utility): add some functions

--- project2/part2/utils.py
+++ project2/part2/utils.py
@@ -100,6 +100,7 @@ fc = 4_000
 t = np.arange(0, 1, 1 / f)
 baseband = np.array([-1, -1, -1, 1, 1, 1])
 bit_len = len(baseband)
+CHUNK = 2048
 
 
 def read_data():
@@ -123,6 +124,20 @@ def gen_preamble():
 preamble = gen_preamble()
 
 
+def gen_Mac_frame(payload: list[int],
+                  frame_dest=0,
+                  frame_src=0,
+                  frame_seq: int = 0,
+                  is_ACK=False):
+    if not is_ACK:
+        dest_with_src = [0 for _ in range(8)]
+        frame_type = [0 for _ in range(4)]  #0000 for not ACK
+        seq = [int(x) for x in '{0:08b}'.format(frame_seq)]
+        return dest_with_src + frame_type + seq + payload
+    else:
+        pass
+
+
 def gen_PHY_frame(mac_frame: list[int]) -> np.ndarray:
     frame_wave = np.zeros(len(mac_frame) * bit_len)
     for j in range(len(mac_frame)):
@@ -133,21 +148,62 @@ def gen_PHY_frame(mac_frame: list[int]) -> np.ndarray:
     return np.concatenate([frame_wave_pre, inter_space]).astype(np.float32)
 
 
-def temp(stream_data: bytes):
+def extract_PHY_frame(stream_data: bytes) -> np.ndarray | None:
+    SIMILARITY = 0.4
+    REF: float = np.correlate(preamble, preamble)[0]
+    preamble_len = len(preamble)
     data: np.ndarray = np.frombuffer(stream_data, dtype=np.float32)
-    data_len = len(data)
-    # currently data_len=1024 since we set the CHUNK=1024
-    # the origninal phyframe length is 730
+    # data_len = len(data)
+    # print(len(data))
+    # currently data_len=2048 since we set the CHUNK=2048
+    # the origninal phyframe length is 1180
     # preamble length is 440
-    max_cor = max(
-        np.correlate(data, preamble) / np.correlate(preamble, preamble))
+    cor_arr = np.correlate(data, preamble)
+    max_idx = np.argmax(cor_arr)
+    # print(f'max_id: {max_idx}')
     # print(max_cor)
-    if max_cor > 0:
+    print(f'similarity:{cor_arr[max_idx] / REF}')
+    if (cor_arr[max_idx] / REF > SIMILARITY) and (CHUNK - max_idx) > 1160:
         # this means that we detec a frame
+        # print(f'max_id: {max_idx}')
+        return data[max_idx:max_idx + 1160]
+    else:
+        return None
+
+
+def extract_MAC_frame(phy_frame: np.ndarray) -> np.ndarray | None:
+    # this function first decode phy_frame to binary form
+    SIGNAL_ONE = np.array([-0.5, -0.5, -0.5, 0.5, 0.5, 0.5])
+    frame_without_preamble = phy_frame[len(preamble):]
+    frame_len = int(len(frame_without_preamble) / bit_len)
+    decoded_frame = [
+        (1 if
+         (frame_without_preamble[i * bit_len:(i + 1) * bit_len] @ SIGNAL_ONE) >
+         0 else 0) for i in range(frame_len)
+    ]
+    # then we should check whether this frame is correct by CRC or Hamming
+    pass
+    # after that, the CRC or Hamming code must be removed and return
+    pass
+    return np.array(decoded_frame)
+
+
+def get_ACK_id(mac_frame: np.ndarray) -> int:
+    #this function will return a positive number only if mac_frame is an ACK frame. Otherwise, it will return -1
+    ACK_type = [0, 0, 0, 0]
+    if not (all(mac_frame[8:12] == ACK_type)):
+        return -1
+    else:
+        payload = mac_frame[20:]
+        #calculate ACK_id
         pass
+        return payload[3]
 
-
-# def check_
 
 if __name__ == '__main__':
+    # print(len(preamble))
+    # a = '1010'
+    # print(Hamming.encode(a))
+    # b = '1110010'
+    # print(Hamming.detectError(b))
     pass
