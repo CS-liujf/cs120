cb475c8a6d017a6c18a681855fae88cdfa46dab3
Fly <1756339062@qq.com>
2022-11-05 09:44:37 +0800

    feat(ACK): ack frame

--- project2/part2/utils.py
+++ project2/part2/utils.py
@@ -29,8 +29,6 @@ t = np.arange(0, 1, 1 / f)
 baseband = np.array([-1, -1, -1, 1, 1, 1])
 bit_len = len(baseband)
 CHUNK = 2048
-PREAMBLE_LEN = 440
-MAC_FRAME_LEN = 120
 
 
 def read_data():
@@ -40,6 +38,16 @@ def read_data():
 
         temp = [int(bit) for bit in bit_stream]
         return [temp[i:i + 100] for i in range(0, len(temp), 100)]
+
+
+PREAMBLE_LEN = 440
+MAC_DEST_LEN = 4
+MAC_SRC_LEN = 4
+MAC_TYPE_LEN = 4
+MAC_SEQ_LEN = 10
+MAC_PAYLOAD_LEN = 100
+MAC_HEAD_LEN = MAC_DEST_LEN + MAC_SRC_LEN + MAC_TYPE_LEN + MAC_SEQ_LEN
+MAC_FRAME_LEN = MAC_HEAD_LEN + MAC_PAYLOAD_LEN
 
 
 def gen_preamble():
@@ -54,18 +62,26 @@ def gen_preamble():
 preamble = gen_preamble()
 
 
-def gen_Mac_frame(payload: list[int],
+def gen_Mac_frame(payload: list[int] = None,
                   frame_dest=0,
                   frame_src=0,
                   frame_seq: int = 0,
-                  is_ACK=False):
+                  is_ACK=False,
+                  mac_frame_received=None):
     if not is_ACK:
-        dest_with_src = [0 for _ in range(8)]
-        frame_type = [0 for _ in range(4)]  #0000 for not ACK
+        dest_with_src = [0 for _ in range(MAC_DEST_LEN + MAC_SRC_LEN)]
+        frame_type = [0 for _ in range(MAC_TYPE_LEN)]  #0000 for not ACK
         seq = [int(x) for x in '{0:08b}'.format(frame_seq)]
         return dest_with_src + frame_type + seq + payload
     else:
-        pass
+        dest_with_src = [0 for _ in range(MAC_DEST_LEN + MAC_SRC_LEN)]
+        frame_type = [1 for _ in range(MAC_TYPE_LEN)]
+        seq = [0 for _ in range(MAC_SEQ_LEN)]
+        ack_payload=mac_frame_received[MAC_HEAD_LEN-MAC_SEQ_LEN:MAC_HEAD_LEN]
+        payload = ack_payload + [
+            0 for _ in range(MAC_PAYLOAD_LEN - len(ack_payload))
+        ]
+        return dest_with_src + frame_type + seq + payload
 
 
 def gen_PHY_frame(mac_frame: list[int]) -> np.ndarray:
@@ -80,9 +96,9 @@ def gen_PHY_frame(mac_frame: list[int]) -> np.ndarray:
 
 
 def extract_PHY_frame(stream_data: bytes) -> np.ndarray | None:
-    SIMILARITY = 0.4
+    SIMILARITY = 0.4  #about 0.45
     REF: float = np.correlate(preamble, preamble)[0]
-    PHY_FRAME_LEN = len(preamble) + (MAC_FRAME_LEN + 8) * bit_len
+    PHY_FRAME_LEN = len(preamble) + (MAC_FRAME_LEN + 8) * bit_len  # 8 for CRC8
     data: np.ndarray = np.frombuffer(stream_data, dtype=np.float32)
     # print(len(data))
     # currently data_len=2048 since we set the CHUNK=2048
@@ -91,9 +107,10 @@ def extract_PHY_frame(stream_data: bytes) -> np.ndarray | None:
     max_idx = np.argmax(cor_arr)
     # print(f'max_id: {max_idx}')
     # print(max_cor)
-    print(f'similarity:{cor_arr[max_idx] / REF}')
+    # print(f'similarity:{cor_arr[max_idx] / REF}')
     if (cor_arr[max_idx] / REF >
             SIMILARITY) and (CHUNK - max_idx) > PHY_FRAME_LEN:
+        print(f'similarity:{cor_arr[max_idx] / REF}')
         # this means that we detec a frame
         # print(f'max_id: {max_idx}')
         return data[max_idx:max_idx + PHY_FRAME_LEN]
@@ -121,20 +138,18 @@ def extract_MAC_frame(phy_frame: np.ndarray) -> np.ndarray | None:
 
 def get_ACK_id(mac_frame: np.ndarray) -> int:
     #this function will return a positive number only if mac_frame is an ACK frame. Otherwise, it will return -1
-    ACK_type = [0, 0, 0, 0]
-    if not (all(mac_frame[8:12] == ACK_type)):
+    ACK_type = [1, 1, 1, 1]
+    if not (all(mac_frame[MAC_DEST_LEN + MAC_SRC_LEN:MAC_HEAD_LEN -
+                          MAC_SEQ_LEN] == ACK_type)):
         return -1
     else:
-        payload = mac_frame[20:]
-        #calculate ACK_id
-        pass
-        return payload[3]
+        payload = mac_frame[MAC_HEAD_LEN:]
+        ACK_id = payload[MAC_HEAD_LEN:MAC_HEAD_LEN + 10]  # like [0,1,1,0,0,1]
+        ACK_id = int(''.join(map(str, ACK_id)), 2)
+        print(f'ACK_id:{ACK_id}')
+        return ACK_id
 
 
 if __name__ == '__main__':
     # print(len(preamble))
-    data = [1 for _ in range(120)]
-    frame = CRC8_encode(data)
-    print(len(frame))
-    data2 = [1 for _ in range(128)]
-    print(CRC8_check(frame))
+    print('{0:010b}'.format(2))
