52b815e442f450af820713380aed12232b44a421
Fly <1756339062@qq.com>
2022-11-24 00:21:33 +0800

    feat(nat): read data

---
+++ project3/part2/node1.py
@@ -0,0 +1,64 @@
+from mac_layer import MAC
+from multiprocessing import Queue, Pipe, Process, Barrier
+
+
+def gen_data():
+    temp = [i + 58 for i in range(7)]
+    res = []
+    for i in range(48, 85):
+        if i in temp:
+            continue
+        res.append(40 * chr(i) + '\n')
+    # print(len(res))
+    with open('INPUT.txt', 'w') as f:
+        f.writelines(res)
+
+
+def bits2a(b):
+    return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)] * 8))
+
+
+def read_data():
+    with open('INPUT.txt', 'r') as f:
+        data = f.readlines()
+
+    res = ''
+    for line in data:
+        res += ''.join([
+            '{0:08b}'.format(ord(x))
+            for _, x in enumerate(line[:len(line) - 1])
+        ])
+
+    print(len(res))
+    # print(len(res))
+
+
+class NETWORK(Process):
+    def __init__(self, Transport_Network_queue: Queue) -> None:
+        self.Transport_Network_queue = Transport_Network_queue
+        super().__init__()
+
+    def run(self):
+        Network_Link_queue = Queue()
+        Link_Network_queue = Queue()
+        mac = MAC(Network_Link_queue, Link_Network_queue)
+        mac.start()
+        while True:
+            pass
+
+
+def main():
+    temp = [i + 58 for i in range(7)]
+    res = []
+    for i in range(48, 85):
+        if i in temp:
+            continue
+        res.append(40 * chr(i) + '\n')
+    # print(len(res))
+    with open('INPUT.txt', 'w') as f:
+        f.writelines(res)
+
+
+if __name__ == '__main__':
+    # main()
+    read_data()
