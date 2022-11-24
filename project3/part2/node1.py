f59a54f8d55e3f5916b76ebc2023f8ec89bae71f
Fly <1756339062@qq.com>
2022-11-24 21:21:43 +0800

    feat(part2): add two threads in network layer

--- project3/part2/node1.py
+++ project3/part2/node1.py
@@ -1,6 +1,7 @@
 from mac import MAC
 from multiprocessing import Queue, Pipe, Process, Barrier
-from network_utils import gen_IP_datagram
+from threading import Thread
+from network_utils import gen_IP_datagram, get_IP_payload
 
 
 def gen_data():
@@ -35,6 +36,32 @@ def read_data():
     # print(len(res))
 
 
+class T_MODULE(Thread):
+    def __init__(self, Transport_Network_queue: Queue,
+                 Network_Link_queue: Queue) -> None:
+        self.Transport_Network_queue = Transport_Network_queue
+        self.Network_Link_queue = Network_Link_queue
+        super().__init__()
+
+    def run(self):
+        while True:
+            payload: list[int] = self.Transport_Network_queue.get()
+            ip_datagram = gen_IP_datagram(payload)
+            self.Network_Link_queue.put(ip_datagram)
+
+
+class R_MODULE(Thread):
+    def __init__(self, Link_Network_queue: Queue) -> None:
+        self.Link_Network_queue = Link_Network_queue
+        super().__init__()
+
+    def run(self):
+        while True:
+            # get an ip datagram
+            ip_datagram: list[int] = self.Link_Network_queue.get()
+            get_IP_payload(ip_datagram)
+
+
 class NETWORK(Process):
     def __init__(self, Transport_Network_queue: Queue,
                  Network_Transport_queue: Queue) -> None:
@@ -46,11 +73,12 @@ class NETWORK(Process):
         Network_Link_queue = Queue()
         Link_Network_queue = Queue()
         mac = MAC(Network_Link_queue, Link_Network_queue)
+        t_module = T_MODULE(self.Transport_Network_queue, Network_Link_queue)
+        t_module.start()
+        r_module = R_MODULE(Link_Network_queue)
+        r_module.start()
         mac.start()
-        while True:
-            payload: list[int] = self.Transport_Network_queue.get()
-            ip_datagram = gen_IP_datagram(payload)
-            Network_Link_queue.put(ip_datagram)
+        mac.join()
 
 
 def main():
@@ -61,6 +89,8 @@ def main():
     data_list = read_data()
     for data in data_list:
         Transport_Network_queue.put(data)
+
+    net.join()
     # while True:
     # pass
 
