bcf4e1a01cf7f956ba445167cf758ca80adfc0cf
Fly <1756339062@qq.com>
2022-11-25 10:40:59 +0800

    refactor(part2): move the NETWORK class into a separate file

--- project3/part2/node1.py
+++ project3/part2/node1.py
@@ -1,7 +1,5 @@
-from mac import MAC
-from multiprocessing import Queue, Pipe, Process, Barrier
-from threading import Thread
-from network_utils import gen_IP_datagram, get_IP_payload
+from network import NETWORK
+from multiprocessing import Queue
 
 
 def gen_data():
@@ -14,10 +12,6 @@ def gen_data():
     # print(len(res))
     with open('INPUT.txt', 'w') as f:
         f.writelines(res)
-
-
-def bits2a(b):
-    return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)] * 8))
 
 
 def read_data():
@@ -34,51 +28,6 @@ def read_data():
     temp = [int(bit) for bit in bit_stream]
     return [temp[i:i + PAYLOAD_LEN] for i in range(0, len(temp), PAYLOAD_LEN)]
     # print(len(res))
-
-
-class T_MODULE(Thread):
-    def __init__(self, Transport_Network_queue: Queue,
-                 Network_Link_queue: Queue) -> None:
-        self.Transport_Network_queue = Transport_Network_queue
-        self.Network_Link_queue = Network_Link_queue
-        super().__init__()
-
-    def run(self):
-        while True:
-            payload: list[int] = self.Transport_Network_queue.get()
-            ip_datagram = gen_IP_datagram(payload)
-            self.Network_Link_queue.put(ip_datagram)
-
-
-class R_MODULE(Thread):
-    def __init__(self, Link_Network_queue: Queue) -> None:
-        self.Link_Network_queue = Link_Network_queue
-        super().__init__()
-
-    def run(self):
-        while True:
-            # get an ip datagram
-            ip_datagram: list[int] = self.Link_Network_queue.get()
-            get_IP_payload(ip_datagram)
-
-
-class NETWORK(Process):
-    def __init__(self, Transport_Network_queue: Queue,
-                 Network_Transport_queue: Queue) -> None:
-        self.Transport_Network_queue = Transport_Network_queue
-        self.Network_Transport_queue = Network_Transport_queue
-        super().__init__()
-
-    def run(self):
-        Network_Link_queue = Queue()
-        Link_Network_queue = Queue()
-        mac = MAC(Network_Link_queue, Link_Network_queue)
-        t_module = T_MODULE(self.Transport_Network_queue, Network_Link_queue)
-        t_module.start()
-        r_module = R_MODULE(Link_Network_queue)
-        r_module.start()
-        mac.start()
-        mac.join()
 
 
 def main():
