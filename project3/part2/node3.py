0ce882b8ffdf843f0d30fe74c5e1badd57a2b3e5
Fly <1756339062@qq.com>
2022-11-26 12:36:22 +0800

    fix(part2): node3 binds it's ip and port

--- project3/part2/node3.py
+++ project3/part2/node3.py
@@ -2,7 +2,7 @@ import socket
 
 
 def main():
-    addr = ('127.0.0.1', 10000)
+    addr = ('', 10000)
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(addr)
     count = 0
@@ -10,7 +10,7 @@ def main():
         data, s_addr = udp_socket.recvfrom(100)
         count += 1
         print({'payload': data.decode('utf-8'), 's_addr': s_addr})
-        if count == 10:
+        if count == 30:
             break
 
     with open('./INPUT.txt', 'r') as f:
@@ -18,7 +18,7 @@ def main():
 
     data = [i.rstrip() for i in data]  # remove \n
     for message in data:
-        udp_socket.sendto(message, s_addr)
+        udp_socket.sendto(message.encode('utf-8'), s_addr)
 
     udp_socket.close()
 
