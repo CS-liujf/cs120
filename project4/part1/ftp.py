641b3b50fdaf5baae98b8f991e5bea75cf5585fc
Fly <1756339062@qq.com>
2022-12-15 15:07:01 +0800

    feat(ftp): ftp  sketch

---
+++ project4/part1/ftp.py
@@ -0,0 +1,26 @@
+from ftp_utils import check_input
+import pyfiglet
+
+
+class FTP:
+    def __init__(self, d_addr):
+        result = pyfiglet.figlet_format(text="Athernet  FTP".upper(),
+                                        font="slant")
+        print(result)
+        pass
+
+    def start(self):
+        self.command_input()
+        pass
+
+    def command_input(self) -> str:
+        flag = False
+        while not flag:
+            command_str = input('Please input FTP command: ')
+            flag = check_input(command_str)
+        return command_str
+
+
+if __name__ == '__main__':
+    ftp = FTP()
+    ftp.start()
