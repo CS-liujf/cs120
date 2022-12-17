71df7e4380a0c0d766a0a4732a2441d9cdb77745
Fly <1756339062@qq.com>
2022-12-17 13:07:23 +0800

    feat(node1): node1 sketch

--- project4/part1/node1.py
+++ project4/part1/node1.py
@@ -1,25 +1,4 @@
-from ftp_utils import check_input
-import pyfiglet
-
-
-class FTP:
-    def __init__(self):
-        result = pyfiglet.figlet_format(text="Athernet  FTP".upper(),
-                                        font="slant")
-        print(result)
-        pass
-
-    def start(self):
-        self.command_input()
-        pass
-
-    def command_input(self) -> str:
-        flag = False
-        while not flag:
-            command_str = input('Please input FTP command: ')
-            flag = check_input(command_str)
-        return command_str
-
+from ftp import FTP
 
 if __name__ == '__main__':
     ftp = FTP()
