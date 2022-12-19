cb6f8fe227d4ea7e489ad6369098b31f653218de
Fly <1756339062@qq.com>
2022-12-19 15:57:20 +0800

    fix(tcp): race condition

--- project4/part1/tcp.py
+++ project4/part1/tcp.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process, Manager, Event, Queue, Barrier
+from multiprocessing import Process, Manager, Event, Queue, Barrier, Lock
 from multiprocessing.synchronize import Barrier as Barrier_
 from dataclasses import dataclass
 from queue import Queue as _Queue
@@ -19,51 +19,60 @@ class TCP_ITEM:
 
 class T_PROCESS(Process):
     def __init__(self, tcp_table: dict[str, TCP_ITEM],
-                 Transport_Network_queue: 'Queue[TRAN_NET_ITEM]') -> None:
+                 Transport_Network_queue: 'Queue[TRAN_NET_ITEM]',
+                 tcp_table_lock: Lock) -> None:
         self.tcp_table = tcp_table
         self.Transport_Network_queue = Transport_Network_queue
+        self.tcp_table_lock = tcp_table_lock
         super().__init__()
 
     def run(self):
         while True:
             for key in self.tcp_table.keys():
-                tcp_item = self.tcp_table[key]
-                if len(tcp_item.t_buffer) != 0:
-                    length = min(64, len(tcp_item.t_buffer))
-                    tcp_packet = gen_tcp_packet(
-                        tcp_item.d_addr,
-                        0,
-                        0,
-                        tcp_item.socket.port,
-                        payload=tcp_item.t_buffer[:length])
+                # print(key)
+                # print(self.tcp_table[key])
+                if len(self.tcp_table[key].t_buffer) != 0:
+                    with self.tcp_table_lock:
+                        tcp_item = self.tcp_table[key]
+                        length = min(64, len(tcp_item.t_buffer))
+                        payload = tcp_item.t_buffer[:length]
+                        tcp_item.t_buffer = tcp_item.t_buffer[length:]
+                        self.tcp_table[key] = tcp_item
+                    tcp_packet = gen_tcp_packet(tcp_item.d_addr,
+                                                0,
+                                                0,
+                                                tcp_item.socket.port,
+                                                payload=payload)
                     self.Transport_Network_queue.put_nowait(
                         TRAN_NET_ITEM(tcp_item.socket, tcp_item.d_addr,
                                       tcp_packet, 'TCP'))
-                    tcp_item.t_buffer = tcp_item.t_buffer[length:]
-                    self.tcp_table[key] = tcp_item
 
 
 class R_PROCESS(Process):
     def __init__(self, tcp_table: dict[str, TCP_ITEM],
-                 Network_Transport_queue: 'Queue[bytes]') -> None:
+                 Network_Transport_queue: 'Queue[bytes]',
+                 tcp_table_lock: Lock) -> None:
         self.Network_Transport_queue = Network_Transport_queue
         self.tcp_table = tcp_table
+        self.tcp_table_lock = tcp_table_lock
         super().__init__()
 
     def run(self):
         while True:
             if not self.Network_Transport_queue.empty():
-                tcp_packet = self.Network_Transport_queue.get_nowait()
-                tcp_port = get_tcp_d_port(tcp_packet)
-                tcp_payload = get_tcp_payload(tcp_packet)
-                tcp_item = self.tcp_table[str(tcp_port)]
-                tcp_item.r_buffer = tcp_item.r_buffer + tcp_payload
-                self.tcp_table[str(tcp_port)] = tcp_item
+                with self.tcp_table_lock:
+                    tcp_packet = self.Network_Transport_queue.get_nowait()
+                    tcp_port = get_tcp_d_port(tcp_packet)
+                    tcp_payload = get_tcp_payload(tcp_packet)
+                    tcp_item = self.tcp_table[str(tcp_port)]
+                    tcp_item.r_buffer = tcp_item.r_buffer + tcp_payload
+                    self.tcp_table[str(tcp_port)] = tcp_item
 
 
 class TCP(Process):
     def __init__(self, barrier: Barrier_ = None) -> None:
         self.tcp_table: dict[str, TCP_ITEM] = Manager().dict()
+        self.tcp_table_lock = Lock()
         self.__event = Event()
         self.__barrier = Barrier(6, self.set_status)
         # self.Transport_Network_queue: Queue[bytes] = Queue()
@@ -79,9 +88,11 @@ class TCP(Process):
         network = NETWORK(Transport_Network_queue, Network_Transport_queue,
                           self.__barrier)
         network.start()
-        t_process = T_PROCESS(self.tcp_table, Transport_Network_queue)
+        t_process = T_PROCESS(self.tcp_table, Transport_Network_queue,
+                              self.tcp_table_lock)
         t_process.start()
-        r_process = R_PROCESS(self.tcp_table, Network_Transport_queue)
+        r_process = R_PROCESS(self.tcp_table, Network_Transport_queue,
+                              self.tcp_table_lock)
         r_process.start()
         self.__barrier.wait()
         while True:
@@ -110,25 +121,28 @@ class TCP(Process):
              buffer_size: int = None):
         port = _socket.port
         tcp_item = self.tcp_table[str(port)]
-        if not tcp_item.is_closed:
-            temp = tcp_item.r_buffer
-            if len(temp) == 0:
-                return b''
-            tcp_item.r_buffer = b''
-            self.tcp_table[str(port)] = tcp_item
-            return temp
-
-        return -1
+        if tcp_item.is_closed and len(tcp_item.r_buffer) == 0:
+            return -1
+        elif len(tcp_item.r_buffer) == 0:
+            return b''
+        else:
+            with self.tcp_table_lock:
+                tcp_item = self.tcp_table[str(port)]
+                temp = tcp_item.r_buffer
+                tcp_item.r_buffer = b''
+                self.tcp_table[str(port)] = tcp_item
+                return temp
 
     def write(self, _socket: SOCKET, data: bytes):
         port = _socket.port
-        tcp_item = self.tcp_table[str(port)]
-        if tcp_item.is_connected:
-            tcp_item.t_buffer = tcp_item.t_buffer + data
-            self.tcp_table[str(port)] = tcp_item
-            return 1
+        with self.tcp_table_lock:
+            tcp_item = self.tcp_table[str(port)]
+            if tcp_item.is_connected:
+                tcp_item.t_buffer = tcp_item.t_buffer + data
+                self.tcp_table[str(port)] = tcp_item
+                return 1
 
-        return -1
+            return -1
 
     def get_status(self):
         if self.__event.is_set():
@@ -137,12 +151,15 @@ class TCP(Process):
 
 
 if __name__ == '__main__':
-    import time
-    tcp = TCP()
-    tcp.start()
-    time.sleep(3)
-    _socket = SOCKET('192.168.0.2', 10)
-    d_addr = D_ADDR('192.168.0.1', 10)
-    tcp.connect(d_addr, _socket)
-    tcp.write(_socket, b'abc')
-    tcp.join()
+    # import time
+    # tcp = TCP()
+    # tcp.start()
+    # time.sleep(3)
+    # _socket = SOCKET('192.168.0.2', 10)
+    # d_addr = D_ADDR('192.168.0.1', 10)
+    # tcp.connect(d_addr, _socket)
+    # tcp.write(_socket, b'abc')
+    # tcp.join()
+    temp = {}
+    for key in temp.keys():
+        print(temp[key].i)
